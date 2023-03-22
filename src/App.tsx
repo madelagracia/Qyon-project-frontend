8ef7a3d1a8b41c6007482e266da972010e178d1f
madelagracia <marceladelagracia@gmail.com>
2023-03-22 18:27:20 -0300

    [3] MaterialUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { BrowserRouter } from "react-router-dom"
 import { AppRoutes } from "./routes";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { ThemeProvider } from "@emotion/react";
+import { LightTheme } from "./shared/themes";
+
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
