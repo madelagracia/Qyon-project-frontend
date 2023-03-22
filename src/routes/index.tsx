241ddbf28271019aec237d70c76c9fd784da30ec
madelagracia <marceladelagracia@gmail.com>
2023-03-22 14:45:39 -0300

    [3] Configuration Router Dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,11 @@
-export{ };
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/login" element={<p>Login</p>} />
+
+            <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+    );
+};
